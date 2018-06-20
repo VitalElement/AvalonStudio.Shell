7736d7e9a7d2d99defa3a4c4045ff2eb57367074
Dan Walmsley <dan@walms.co.uk>
2018-06-20 16:49:58 +0100

    Add datacontext provider api.

--- src/AvalonStudio.Shell/Shell.cs
+++ src/AvalonStudio.Shell/Shell.cs
@@ -3,12 +3,13 @@ using Avalonia.Controls;
 using AvalonStudio.Extensibility;
 using AvalonStudio.Shell.Extensibility.Platforms;
 using Dock.Model;
+using System;
 
 namespace AvalonStudio.Shell
 {
 	public static class Shell
 	{
-		public static void StartShellApp<TAppBuilder, TMainWindow>(this TAppBuilder builder, string appName, IDockFactory layoutFactory = null) where TAppBuilder : AppBuilderBase<TAppBuilder>, new() where TMainWindow : Window, new()
+		public static void StartShellApp<TAppBuilder, TMainWindow>(this TAppBuilder builder, string appName, IDockFactory layoutFactory = null, Func<object> dataContextProvider = null) where TAppBuilder : AppBuilderBase<TAppBuilder>, new() where TMainWindow : Window, new()
 		{
 			builder.UseReactiveUI().AfterSetup(_ =>
 			{
@@ -23,25 +24,12 @@ namespace AvalonStudio.Shell
 				ShellViewModel.Instance = IoC.Get<ShellViewModel>();
 
 				ShellViewModel.Instance.Initialise(layoutFactory);
-			}).Start<TMainWindow>();
+			}).Start<TMainWindow>(dataContextProvider);
 		}
 
-		public static void StartShellApp<TAppBuilder>(this TAppBuilder builder, string appName, IDockFactory layoutFactory = null) where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
+		public static void StartShellApp<TAppBuilder>(this TAppBuilder builder, string appName, IDockFactory layoutFactory = null, Func<object> dataContextProvider = null) where TAppBuilder : AppBuilderBase<TAppBuilder>, new()
 		{
-			builder.UseReactiveUI().AfterSetup(_ =>
-			{
-				Platform.AppName = appName;
-				Platform.Initialise();
-
-				var extensionManager = new ExtensionManager();
-				var container = CompositionRoot.CreateContainer(extensionManager);
-
-				IoC.Initialise(container);
-
-				ShellViewModel.Instance = IoC.Get<ShellViewModel>();
-
-				ShellViewModel.Instance.Initialise(layoutFactory);
-			}).Start<MainWindow>();
+			StartShellApp<TAppBuilder>(builder, appName, layoutFactory, dataContextProvider);
 		}
 	}
 }
