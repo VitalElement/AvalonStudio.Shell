abadb8f8bd6a6520c06bfa0d7bb52109130da948
Dan Walmsley <dan@walms.co.uk>
2018-06-20 14:38:02 +0100

    dont crash if a command is not found.

--- src/AvalonStudio.Shell.Extensibility/Commands/CommandService.cs
+++ src/AvalonStudio.Shell.Extensibility/Commands/CommandService.cs
@@ -6,80 +6,83 @@ using AvalonStudio.Commands.Settings;
 
 namespace AvalonStudio.Commands
 {
-    [Export]
-    [Shared]
-    public class CommandService
-    {
-        private readonly CommandSettingsService _commandSettingsService;
-        private readonly IEnumerable<Lazy<CommandDefinition, CommandDefinitionMetadata>> _commands;
+	[Export]
+	[Shared]
+	public class CommandService
+	{
+		private readonly CommandSettingsService _commandSettingsService;
+		private readonly IEnumerable<Lazy<CommandDefinition, CommandDefinitionMetadata>> _commands;
 
-        private readonly Lazy<IImmutableDictionary<string, Lazy<CommandDefinition>>> _resolvedCommands;
+		private readonly Lazy<IImmutableDictionary<string, Lazy<CommandDefinition>>> _resolvedCommands;
 
-        private IImmutableDictionary<CommandDefinition, IEnumerable<string>> _keyGestures;
+		private IImmutableDictionary<CommandDefinition, IEnumerable<string>> _keyGestures;
 
-        [ImportingConstructor]
-        public CommandService(
-            CommandSettingsService commandSettingsService,
-            [ImportMany] IEnumerable<Lazy<CommandDefinition, CommandDefinitionMetadata>> commands)
-        {
-            _commandSettingsService = commandSettingsService;
-            _commands = commands;
+		[ImportingConstructor]
+		public CommandService(
+			CommandSettingsService commandSettingsService,
+			[ImportMany] IEnumerable<Lazy<CommandDefinition, CommandDefinitionMetadata>> commands)
+		{
+			_commandSettingsService = commandSettingsService;
+			_commands = commands;
 
-            _resolvedCommands = new Lazy<IImmutableDictionary<string, Lazy<CommandDefinition>>>(ResolveCommands);
-        }
+			_resolvedCommands = new Lazy<IImmutableDictionary<string, Lazy<CommandDefinition>>>(ResolveCommands);
+		}
 
-        public Lazy<CommandDefinition> GetCommand(string commandName)
-        {
-            var resolvedCommands = _resolvedCommands.Value;
+		public Lazy<CommandDefinition> GetCommand(string commandName)
+		{
+			var resolvedCommands = _resolvedCommands.Value;
 
-            if (!resolvedCommands.TryGetValue(commandName, out var command))
-            {
-                // todo: log warning
-            }
+			if (!resolvedCommands.TryGetValue(commandName, out var command))
+			{
+				// todo: log warning
+			}
 
-            return command;
-        }
+			return command;
+		}
 
-        public IImmutableDictionary<CommandDefinition, IEnumerable<string>> GetKeyGestures()
-        {
-            if (_keyGestures != null)
-            {
-                return _keyGestures;
-            }
+		public IImmutableDictionary<CommandDefinition, IEnumerable<string>> GetKeyGestures()
+		{
+			if (_keyGestures != null)
+			{
+				return _keyGestures;
+			}
 
-            var commandSettings = _commandSettingsService.GetCommandSettings();
-            var builder = ImmutableDictionary.CreateBuilder<CommandDefinition, IEnumerable<string>>();
+			var commandSettings = _commandSettingsService.GetCommandSettings();
+			var builder = ImmutableDictionary.CreateBuilder<CommandDefinition, IEnumerable<string>>();
 
-            foreach (var command in _commands)
-            {
-                if (!commandSettings.Commands.TryGetValue(command.Metadata.Name, out var settings))
-                {
-                    settings = new Command();
+			foreach (var command in _commands)
+			{
+				if (command.Value != null)
+				{
+					if (!commandSettings.Commands.TryGetValue(command.Metadata.Name, out var settings))
+					{
+						settings = new Command();
 
-                    if (command.Metadata.DefaultKeyGestures != null)
-                    {
-                        settings.KeyGestures.AddRange(command.Metadata.DefaultKeyGestures);
-                    }
+						if (command.Metadata.DefaultKeyGestures != null)
+						{
+							settings.KeyGestures.AddRange(command.Metadata.DefaultKeyGestures);
+						}
 
-                    commandSettings.Commands.Add(command.Metadata.Name, settings);
-                }
+						commandSettings.Commands.Add(command.Metadata.Name, settings);
+					}
 
-                builder.Add(command.Value, settings.KeyGestures);
-            }
+					builder.Add(command.Value, settings.KeyGestures);
+				}
+			}
 
-            return _keyGestures = builder.ToImmutable();
-        }
+			return _keyGestures = builder.ToImmutable();
+		}
 
-        private IImmutableDictionary<string, Lazy<CommandDefinition>> ResolveCommands()
-        {
-            var builder = ImmutableDictionary.CreateBuilder<string, Lazy<CommandDefinition>>();
+		private IImmutableDictionary<string, Lazy<CommandDefinition>> ResolveCommands()
+		{
+			var builder = ImmutableDictionary.CreateBuilder<string, Lazy<CommandDefinition>>();
 
-            foreach (var command in _commands)
-            {
-                builder.Add(command.Metadata.Name, command);
-            }
+			foreach (var command in _commands)
+			{
+				builder.Add(command.Metadata.Name, command);
+			}
 
-            return builder.ToImmutable();
-        }
-    }
+			return builder.ToImmutable();
+		}
+	}
 }
