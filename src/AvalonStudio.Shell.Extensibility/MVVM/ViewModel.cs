04bb3ac761940853d3b16d0306540920cfbea3d2
Dan Walmsley <dan@walms.co.uk>
2018-07-19 14:55:27 +0100

    restore default locations.

--- src/AvalonStudio.Shell.Extensibility/MVVM/ViewModel.cs
+++ src/AvalonStudio.Shell.Extensibility/MVVM/ViewModel.cs
@@ -8,11 +8,6 @@ namespace AvalonStudio.MVVM
         Left,
         Right,
         Bottom,
-        BottomRight,
-        RightBottom,
-        RightMiddle,
-        RightTop,
-        MiddleTop,
     }
 
     public abstract class ViewModel : ViewModel<object>
