d27bbeb7273ad5a0d31fd4181be63b695b5095fd
José Pedro <jpedrolmail@gmail.com>
2018-09-01 22:27:53 +0100

    Changed the composition engine to Microsoft.VisualStudio.Composition.

--- src/AvalonStudio.Shell/CompositionRoot.cs
+++ src/AvalonStudio.Shell/CompositionRoot.cs
@@ -1,29 +1,88 @@
 using AvalonStudio.Extensibility;
 using AvalonStudio.Extensibility.Utils;
+using Microsoft.VisualStudio.Composition;
 using System.Collections.Generic;
-using System.Composition.Convention;
-using System.Composition.Hosting;
+using System.IO;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AvalonStudio
 {
     public static class CompositionRoot
     {
-        public static CompositionHost CreateContainer(ExtensionManager extensionManager)
+        public static async Task<ExportProvider> CreateExportProviderAsync(
+            ExtensionManager extensionManager,
+            CancellationToken cancellationToken = default)
         {
-            var conventions = new ConventionBuilder();
-            conventions.ForTypesDerivedFrom<IExtension>().Export<IExtension>();
+            var resolver = Resolver.DefaultInstance;
 
-            // TODO AppDomain here is a custom appdomain from namespace AvalonStudio.Extensibility.Utils. It is able
-            // to load any assembly in the bin directory (so not really appdomain) we need to get rid of this
-            // once all our default extensions are published with a manifest and copied to extensions dir.
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            var extensionAssemblies = LoadMefComponents(extensionManager);
+            ComposableCatalog catalog;
 
-            var configuration = new ContainerConfiguration()
-                .WithAssemblies(assemblies, conventions)
-                .WithAssemblies(extensionAssemblies);
-            return configuration.CreateContainer();
+            var cachedCatalog = new CachedCatalog();
+
+            var cacheIsValid = false;
+            var cachePath = "";
+
+            if (cacheIsValid)
+            {
+                using (var cacheStream = File.OpenRead(cachePath))
+                {
+                    catalog = await cachedCatalog.LoadAsync(cacheStream, resolver, cancellationToken);
+                }
+            }
+            else
+            {
+                var discovery = new AttributedPartDiscovery(resolver, true);
+
+                // TODO AppDomain here is a custom appdomain from namespace AvalonStudio.Extensibility.Utils. It is able
+                // to load any assembly in the bin directory (so not really appdomain) we need to get rid of this
+                // once all our default extensions are published with a manifest and copied to extensions dir.
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                var extensionAssemblies = LoadMefComponents(extensionManager);
+
+                catalog = ComposableCatalog.Create(resolver)
+                    .AddParts(await discovery.CreatePartsAsync(assemblies).ConfigureAwait(false))
+                    .AddParts(await discovery.CreatePartsAsync(extensionAssemblies).ConfigureAwait(false));
+
+                // TODO: cache catalog
+                //using (var cacheStream = File.OpenWrite(cachePath))
+                //{
+                //    await cachedCatalog.SaveAsync(catalog, cacheStream, cancellationToken);
+                //}
+            }
+
+            var configuration = CompositionConfiguration.Create(catalog);
+
+            // TODO: log errors to file
+
+            var compositionErrors = configuration.CompositionErrors;
+
+            while (!compositionErrors.IsEmpty)
+            {
+                var error = compositionErrors.Peek();
+
+                System.Console.WriteLine();
+                System.Console.WriteLine("Composition Error:");
+                System.Console.WriteLine();
+
+                foreach (var entry in error)
+                {
+                    System.Console.WriteLine(entry.Message);
+
+                    foreach (var part in entry.Parts)
+                    {
+                        System.Console.WriteLine($"    Part: {part.Definition.Id}");
+                    }
+                }
+
+                compositionErrors = compositionErrors.Pop();
+            }
+
+            var exportProviderFactory = configuration.CreateExportProviderFactory();
+            var exportProvider = exportProviderFactory.CreateExportProvider();
+
+            return exportProvider;
         }
 
         private static IEnumerable<Assembly> LoadMefComponents(ExtensionManager extensionManager)
