53a603232ea0602531057c8abe963ed88093b65b
Dan Walmsley <dan@walms.co.uk>
2019-03-24 20:40:16 +0000

    fix nre in command menu item, when command is malformed.

--- src/AvalonStudio.Shell.Extensibility/Menus/CommandMenuItem.cs
+++ src/AvalonStudio.Shell.Extensibility/Menus/CommandMenuItem.cs
@@ -7,10 +7,10 @@ namespace AvalonStudio.Menus
 {
     internal class CommandMenuItem : IMenuItem
     {
-        public string Label => _commandDefinition.Value.Label;
-        public DrawingGroup Icon => _commandDefinition.Value.Icon;
+        public string Label => _commandDefinition.Value?.Label;
+        public DrawingGroup Icon => _commandDefinition.Value?.Icon;
 
-        public ICommand Command => _commandDefinition.Value.Command;
+        public ICommand Command => _commandDefinition.Value?.Command;
 
         private readonly CommandService _commandsService;
         private readonly string _commandName; 
@@ -25,6 +25,6 @@ namespace AvalonStudio.Menus
             _commandDefinition = new Lazy<CommandDefinition>(ResolveCommandDefinition);
         }
 
-        private CommandDefinition ResolveCommandDefinition() => _commandsService.GetCommand(_commandName).Value;
+        private CommandDefinition ResolveCommandDefinition() => _commandsService.GetCommand(_commandName)?.Value;
     }
 }
