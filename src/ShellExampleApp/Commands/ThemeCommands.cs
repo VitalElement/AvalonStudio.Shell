8ece4e7076fcc978b37475cf6cbca97f3d7f0978
Marcel <nitan.marcel@gmail.com>
2020-12-28 00:26:08 +0200

    Example of runtime theme changing and saving to GlobalSettings

---
+++ src/ShellExampleApp/Commands/ThemeCommands.cs
@@ -0,0 +1,46 @@
+﻿using Avalonia.Threading;
+using AvalonStudio.Commands;
+using AvalonStudio.Extensibility;
+using AvalonStudio.Extensibility.Theme;
+using AvalonStudio.GlobalSettings;
+using AvalonStudio.Shell;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+
+namespace ShellExampleApp.Commands
+{
+    internal class ThemeCommands
+    {
+        [ExportCommandDefinition("Theme.Light")]
+        public CommandDefinition LightCommand { get; }
+
+        [ExportCommandDefinition("Theme.Dark")]
+        public CommandDefinition DarkCommand { get; }
+
+        private IShell _shell;
+
+        [ImportingConstructor]
+        public ThemeCommands(CommandIconService commandIconService)
+        {
+            _shell = IoC.Get<IShell>();
+
+            LightCommand = new CommandDefinition("Light", null, ReactiveCommand.Create(SetThemeLight));
+            DarkCommand = new CommandDefinition("Dark", null, ReactiveCommand.Create(SetThemeDark));
+        }
+
+        public static void SetThemeLight()
+        {
+            Settings.SetSettings(new GeneralSettings() { Theme = ColorTheme.VisualStudioLight.Name });
+            ColorTheme.LoadTheme(ColorTheme.VisualStudioLight);
+        }
+
+        public static void SetThemeDark()
+        {
+            Settings.SetSettings(new GeneralSettings() { Theme = ColorTheme.VisualStudioDark.Name });
+            ColorTheme.LoadTheme(ColorTheme.VisualStudioDark);
+        }
+    }
+}
---
+++ src/ShellExampleApp/MainMenu/ThemeMainMenuItem.cs
@@ -0,0 +1,42 @@
+﻿using AvalonStudio.MainMenu;
+using AvalonStudio.Menus;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+
+namespace ShellExampleApp.MainMenu
+{
+    internal class ThemeMainMenuItems
+    {
+        [ExportMainMenuItem("Theme")]
+        [DefaultOrder(2000)]
+        public IMenuItem Help => _menuItemFactory.CreateHeaderMenuItem("Theme", null);
+
+        [ExportMainMenuDefaultGroup("Theme", "Light")]
+        [DefaultOrder(0)]
+        public object LightGroup => null;
+
+        [ExportMainMenuDefaultGroup("Theme", "Dark")]
+        [DefaultOrder(0)]
+        public object DarkGroup => null;
+
+        [ExportMainMenuItem("Theme", "Light")]
+        [DefaultOrder(0)]
+        [DefaultGroup("Light")]
+        public IMenuItem Light => _menuItemFactory.CreateCommandMenuItem("Theme.Light");
+
+        [ExportMainMenuItem("Theme", "Dark")]
+        [DefaultOrder(0)]
+        [DefaultGroup("Light")]
+        public IMenuItem Dark => _menuItemFactory.CreateCommandMenuItem("Theme.Dark");
+
+        private IMenuItemFactory _menuItemFactory;
+
+        [ImportingConstructor]
+        public ThemeMainMenuItems(IMenuItemFactory menuItemFactory)
+        {
+            _menuItemFactory = menuItemFactory;
+        }
+    }
+}
